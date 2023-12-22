6b6955fb46867dd9685464d73f42d6aa1e4dbd31
ephraaimwong <73452838+ephraaimwong@users.noreply.github.com>
2023-12-22 13:43:33 -0600

    Delete docs directory

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.ou.cs2334.project5"},{"l":"edu.ou.cs2334.project5.handlers"},{"l":"edu.ou.cs2334.project5.interfaces"},{"l":"edu.ou.cs2334.project5.models"},{"l":"edu.ou.cs2334.project5.presenters"},{"l":"edu.ou.cs2334.project5.views"}];updateSearchResults();
